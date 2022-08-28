319e58541df321b866079d4d15712cd68f60a359
Eren Şapçı <sapci.erenn@gmail.com>
2022-08-28 15:31:24 +0300

    GroupingController Added

---
+++ WasteCollectionSystem/Controllers/GroupingController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WasteCollectionSystem.Context;
+using WasteCollectionSystem.Models;
+
+namespace WasteCollectionSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupingController : ControllerBase
+    {
+        private readonly IMapperSession session;
+        public GroupingController(IMapperSession session)
+        {
+            this.session = session;
+        }
+        [HttpGet]
+        public List<List<Container>> groups (long vehicleId, int numberOfGroups)
+        {
+   
+            var groups = new List<List<Container>>();
+            List<Container> containerList = session.Containers.Where(x => x.VehicleId == vehicleId).ToList();
+            for (int i=0; i<numberOfGroups;i++)
+            {
+                groups.Add(new List<Container>());
+            }
+            for (int j=0; j<containerList.Count;j++) 
+            {
+                var count = j;
+                j = numberOfGroups * count;
+                
+                for (int i = 0; i < numberOfGroups; i++)
+                {                  
+                    if (count < containerList.Count)
+                    {
+                        groups[i].Add(containerList[count]);
+                        count++;
+                    }
+                    else break;
+                } 
+            }
+            return groups;
+        }
+    }
+}
